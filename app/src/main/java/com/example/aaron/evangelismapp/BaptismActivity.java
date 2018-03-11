702b3aa3a9b6cbda4942448d8483fe8d21ff3240
uenvymenow <uenvymenow@gmail.com>
2018-03-11 15:55:14 -0500

    Base of application with main textviews as well as activities created

---
+++ app/src/main/java/com/example/aaron/evangelismapp/BaptismActivity.java
@@ -0,0 +1,13 @@
+package com.example.aaron.evangelismapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaptismActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baptism);
+    }
+}
