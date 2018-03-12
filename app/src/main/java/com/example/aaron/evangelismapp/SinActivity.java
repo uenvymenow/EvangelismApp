e5ed9d7c7ee2cf630800ebe3ffdf21b38482ed65
uenvymenow <uenvymenow@gmail.com>
2018-03-11 23:02:36 -0500

    Added skeleton layout for SinActivity

--- app/src/main/java/com/example/aaron/evangelismapp/SinActivity.java
+++ app/src/main/java/com/example/aaron/evangelismapp/SinActivity.java
@@ -2,6 +2,7 @@ package com.example.aaron.evangelismapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SinActivity extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class SinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sin);
+
+        // Locate the SinWordTextView
+        TextView sinWord = findViewById(R.id.sinWordTextView);
+
+        // Add the word Sin to the textview
+        sinWord.setText("Sin");
+
+        // Locate the sinScriptureTextView
+        TextView sinScripture = findViewById(R.id.sinScriptureTextView);
+
+        // Add the scripture reference to the sinScriptureTextView
+        sinScripture.setText("Scripture 1");
+
+        // Locate the sinImageView
+        ImageView sinImage = findViewById(R.id.sinImage);
+
+        // Add the sin image to the sinImageView
+        sinImage.setImageResource(R.drawable.sinimage);
     }
 }
